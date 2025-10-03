102516756403e35d5b5436a8f8a8848a1dc126b5
Thirumalayvel <thirumalayvel7@gmail.com>
2025-10-03 16:31:12 +0530

    find winning move

--- Tic_Tac_Toe.js
+++ Tic_Tac_Toe.js
@@ -34,9 +34,36 @@ function twoIndexExcept(index){
     else if(index==1) return [0,2];
     else return [0,1];
 }
+function findWinIndex(myMoves,opponentMoves){
+    let opsRows=[],opsColumns=[]; // to note filled opponents rows and columns
+    for(let i=0;i<opponentMoves.length;i++){   
+        opsRows.push(opponentMoves[i].row);
+        opsColumns.push(opponentMoves[i].column);
+    }
+
+    let rowFinder=(i1,i2)=>{
+        if(myMoves[i1].row==myMoves[i2].row && !opsRows.includes(myMoves[i1].row)) return true;
+        else false;
+    }
+    console.log(slidingWindowLoop(rowFinder,myMoves));
+}
+function slidingWindowLoop(logicFunction,arrayObjects){
+    for(let i=1;i<arrayObjects.length;i++){
+        for(let j=i;j<arrayObjects.length;j++){
+            if(logicFunction(j,j-i)) return [j,j-i];
+        }
+    }
+} 
 function findChances(selectedMove,myMoves,opponentMoves){
 
+
 }
+
+
+
+
+
+
 function out(x){
     console.log(x);
 }
@@ -44,8 +71,18 @@ out(twoIndexExcept(0));
 out(twoIndexExcept(1));
 out(twoIndexExcept(2) );
 
-
-
+let myMoves=[
+    new xoElement(0,0),
+    new xoElement(2,0),
+    new xoElement(2,2)
+];
+let opponentMoves=[
+    new xoElement(0,2),
+    new xoElement(1,1),
+];
+findWinIndex(myMoves,opponentMoves);
+opponentMoves[1]=new xoElement(2,1);
+findWinIndex(myMoves,opponentMoves);
 
 
 
